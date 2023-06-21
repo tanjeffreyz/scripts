5da8fc495a91309a380f3151fffa555d2ac5748d
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2023-06-21 14:51:25 -0700

    added description transfer

--- swagger_scripts/to_swagger/to_swagger.py
+++ swagger_scripts/to_swagger/to_swagger.py
@@ -46,15 +46,38 @@ def convert(obj, set_example):
     return converted
 
 
+def transfer(obj, prev):
+    if type(obj) is list and type(prev) is list:
+        # Map properties of prev by name
+        prev_map = {x['name']: x for x in prev if 'name' in x}
+
+        # Only recurse on properties in obj that are also preset in prev
+        for i in range(len(obj)):
+            if type(obj[i]) is dict and 'name' in obj[i]:
+                name = obj[i]['name']
+                if name in prev_map:
+                    transfer(obj[i], prev_map[name])
+    elif type(obj) is dict and type(prev) is dict:
+        if 'description' in prev:
+            desc = prev['description']
+            if type(desc) is str:
+                obj['description'] = desc
+        for key in obj:
+            if key in prev:
+                transfer(obj[key], prev[key])
+
+
 if __name__ == '__main__':
-    #############
-    #   Name    #
-    #############
-    if not os.path.exists('name.txt'):
-        with open('name.txt', 'w') as file:
-            file.write('default')
-    with open('name.txt', 'r') as file:
-        name = file.read()
+    if not os.path.exists('prev.py'):
+        with open('prev.py', 'w') as file:
+            file.write('default = {}\n')
+
+    import prev
+    variables = [x for x in dir(prev) if not x.startswith('_')]
+    assert len(variables) == 1, 'prev.py must only contain 1 variable'
+    name = variables[0]
+    prev_swagger = prev.__dict__[name]
+    assert type(prev_swagger) is dict, 'prev.py must contain a dictionary'
 
     #############
     #    URL    #
@@ -137,9 +160,9 @@ if __name__ == '__main__':
     cleaned = clean(response)
     schema = convert(cleaned, True)
     result = {
-        'tags': [],
-        'summary': '',
-        'description': '',
+        'tags': prev_swagger.get('tags', ['']),
+        'summary': prev_swagger.get('summary', ''),
+        'description': prev_swagger.get('description', ''),
         'consumes': [consumes],
         'parameters': parameters,
         'responses': {
@@ -149,6 +172,7 @@ if __name__ == '__main__':
             }
         }
     }
+    transfer(result, prev_swagger)
     string = (
         json.dumps(result, indent=4)
         .replace('true', 'True')
@@ -157,4 +181,4 @@ if __name__ == '__main__':
     )
 
     with open('out.py', 'w') as file:
-        file.write(f'{name.strip()}_swagger = ' + string.strip() + '\n')
+        file.write(f'{name.strip()} = ' + string.strip() + '\n')
